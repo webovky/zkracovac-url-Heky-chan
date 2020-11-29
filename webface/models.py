389785ce385adc6cb7beaeac129a636068b2d198
Heky-chan <71437946+Heky-chan@users.noreply.github.com>
2020-11-29 13:50:23 +0100

    Add files via upload
    Trvalo mi seznámit se s novými příkazy a mé pracovní podmínky také nepomáhaly. Snažil jsem se využít věci, které jsme probírali v minulých projektech. Práce mě rozhodně poučila ve způsobech přihlašování a uchovávání uživatelů. Práce není dokonalá a tak bych ji ohodnotil za 2.

--- webface/models.py
+++ webface/models.py
@@ -1,12 +1,7 @@
-# from datetime import datetime
-from pony.orm import PrimaryKey, Required, Optional, Set
+from pony.orm import PrimaryKey, Required, Optional, Set, Database
 
-# Import Flask-Pony instance from __init__.py module
-from . import pony
-
-# Get a reference to the base class of Pony entities
-db = pony.db
-
+db = Database()
+db.bind(provider="sqlite", filename="database.sqlite", create_db=True)
 
 class User(db.Entity):
     login = PrimaryKey(str)
@@ -19,3 +14,5 @@ class Shortener(db.Entity):
     shortcut = PrimaryKey(str)
     url = Required(str)
     user = Optional(User)
+
+db.generate_mapping(create_tables=True)
--- webface/routes.py
+++ webface/routes.py
@@ -1,10 +1,13 @@
-from . import app
-from flask import render_template, request, redirect, url_for, session
+######################################################################################
+import string
+import random
 import functools
-
-# from werkzeug.security import check_password_hash
-
-
+from . import app
+from pony.orm import db_session
+from .models import User, Shortener
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import render_template, request, redirect, url_for, session, flash
+######################################################################################
 def login_required(function):
     @functools.wraps(function)
     def wrapper(*args, **kwargs):
@@ -12,30 +15,68 @@ def login_required(function):
             return function(*args, **kwargs)
         else:
             return redirect(url_for("login", url=request.path))
-
     return wrapper
-
-
+######################################################################################
 @app.route("/", methods=["GET"])
 def index():
+    shortcut = "".join([random.choice(string.ascii_letters) for i in range(7)])
+    print(shortcut)
+######################################################################################
+    if "user" in session:
+        user = User.get(login=session["user"])
+        for addr in user.addresses:
+            print(addr.shortcut, addr.url)
     return render_template("base.html.j2")
-
-
+######################################################################################
 @app.route("/<string:shortcut>", methods=["GET"])
 def short_redirect(shortcut):
+    shortener = Shortener.get(shortcut=shortcut)
+    if shortener.user:
+        print(shortener.user.login)
     return render_template("base.html.j2")
-
-
-@app.route("/adduser/")
+######################################################################################
+@app.route("/adduser/", methods=["GET"])
 def adduser():
-    return render_template("base.html.j2")
-
-
+    return render_template("adduser.html.j2")
+######################################################################################
+@app.route("/adduser/", methods=["POST"])
+@db_session
+def adduser_post():
+    login = None
+    passwd1 = None
+    passwd2 = None
+    user = User.get(email="nozka@spseol.cz")
+    user = User[login]
+    if user:
+        flash("Daný uživatel již existuje")
+        print(user.login, user.password)
+    if len(passwd1) >= 6 and passwd1 == passwd2:
+        user = User(login=login, password=generate_password_hash(passwd1))
+        flash("účet vytvořen")
+    else:
+        flash("Hesla se neshodují nebo jsou příliš krátká")
+        return redirect(url_for("adduser"))
+    return redirect(url_for("index"))
+######################################################################################
 @app.route("/login/")
 def login():
     return render_template("login.html.j2")
-
-
+######################################################################################
+@app.route("/login/", methods=["POST"])
+def login_post():
+    login = None
+    passwd = None
+    user = User[login]
+    if user and check_password_hash(user.password, passwd):
+        session["user"] = login
+        flash("Přihlášení proběhlo úspěšně")
+    else:
+        flash("Nezprávné údaje")
+    return redirect(url_for("index"))
+######################################################################################
 @app.route("/logout/")
 def logout():
+    session.pop("user", None)
+    flash("Odhlášení proběhlo úspěšně")
     return render_template("base.html.j2")
+######################################################################################
